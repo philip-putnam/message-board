ed76e1841fd1f2ac07c00856f0bfa1082b1049ce
Philip Putnam <putnamp989@gmail.com>
2017-03-24 11:37:05 -0700

    add answer model, output all answers on dynamic question pages

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('question', params.question_id);
+    console.log(this.store.findRecord('question', params.question_id));
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id),
+      answers: this.store.findAll('answer')
+    });
   },
 });
